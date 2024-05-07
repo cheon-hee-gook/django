cf13b35dfc0c00df61c3def7027752659d044724
hg <chg9503@naver.com>
2024-05-07 15:13:05 +0900

    polls app - change to class view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,21 +1,44 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.views import generic
+
 from polls.models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-        }
+
+# def index(request):
+#     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list
+#         }
     
-    return render(request, 'polls/index.html', context)
+#     return render(request, 'polls/index.html', context)
+# -- Class-based GenericView
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# -- Function-based View
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -32,7 +55,3 @@ def vote(request, question_id):
         # POST 데이터를 정상적으로 처리하였으면,
         # 항상 HttpResponseRedirect를 반환하여 리다이렉션 처리함
         return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
-    
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
