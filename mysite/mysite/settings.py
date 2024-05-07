ab6d3fc49886bc3b1151ee57c13657f44ab9669d
hg <chg9503@naver.com>
2024-05-07 14:49:34 +0900

    apps.py - using verbose_name views.py - using path, label

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'books.apps.BooksConfig',   # 추가
+    'books.apps.BooksConfig',
 ]
 
 MIDDLEWARE = [
